53323072d6863ed135aeb76f24dd2a8c37a95ee7
gcollias <145624169+gcollias@users.noreply.github.com>
2023-09-25 17:29:48 -0700

    Update vbsme(code snipbit).v

--- vbsme(code snipbit).v
+++ vbsme(code snipbit).v
@@ -8,24 +8,29 @@ vbsme:
     li $t0, 1                       # variable for row;      i(row) = 1
     li $t1, 1                       # variable for column;   i(col) = 1
 
-    lw $t2, 0($a0)                  # row size
-    lw $t3, 4($a0)                  # column size
+    lw $t2, 0($a0)                  # frame row size
+    lw $t3, 4($a0)                  # frame column size
+
+    lw $t6, 8($a0)                  # window row size
+    lw $t7, 12($a0)                 # window column size 
 
     mul $t4, $t2, $t3               # frame (row * column)
+    mul $t7, $t6, $t7               # frame (row * column)
 
     li $t5, 0                       # nunElements = 0
 
-    loop: bgeq $t5, $t4, exit       # numElements >= (row * column)
+    loop: slt $t4, $t5, exit       # numElements >= (row * column)
 
-    addi $sp, $sp, ?                # create space on stack pointer
+    addi $sp, $sp, -4                # create space on stack pointer
     sw $ra, 0($sp)                  # save return address
-    subi $s0, $t0, 1                # parameters for row (i-l)
-    subi $s1, $t1, 1                # parameters for column (j-l)
+    addi $s0, $t0, -1                # parameters for row (i-l)
+    addi $s1, $t1, -1                # parameters for column (j-l)
 
     jal SAD                         # calculate SAD by calling SAD function
 
     lw $ra, 0($sp)                  # restore the return address
-    addi $sp, $sp, ?                # restore the stack pointer
+    addi $sp, $sp, 4                # restore the stack pointer
 
+    exit:                            #exit loop
     jr $ra
    
