1c9ad793f454cf53ebf3194848c4cd2d68c2b477
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-09-26 11:13:51 -0700

    Update vbsmeattempt.v

--- vbsmeattempt.v
+++ vbsmeattempt.v
@@ -29,7 +29,7 @@ vbsme:
     #li $t0, 1                       # variable for row;      i(row) = 1
     la $t0, window0
     la $t1, frame0
-    la $t2, window5 #Change $t2 right now because this is very incorrect
+    la $t2, window5                  #Change $t2 right now because this is very incorrect
     #li $t1, 1                       # variable for column;   i(col) = 1
 
     addi $t5, $zero, 0                # parameters for row (i-l) for loop
@@ -47,31 +47,32 @@ vbsme:
     #li $t5, 0                       # nunElements = 0
 
 
-#    loop: beq $t5, $t4, exit       # numElements >= (row * column)
+    #loop: beq $t5, $t4, exit       # numElements >= (row * column)
 
 
     loopi:
-    addi   $t6, $zero, 0     # parameters for column (j-l) for loop
+    addi   $t6, $zero, 0         # parameters for column (j-l) for loop
     jal    loopj                 # starts inner loopj
     addi   $t5, $t5, 1           # i++
-    bne    $s4, $t5, loopi      # continue loopi if i < window row size
+    bne    $s4, $t5, loopi       # continue loopi if i < window row size
 
     #Before printing original box move window over and repeat loops
     #increase the address of loops? how? I think its t9 and t4
     #why does c address always throw error? 
+        
     #  li  $t7, 0              # reset t7 to 0 and increase address?
     # # addi $t7, $t7, 2
     #  li  $t4, 0              # reset t4 to 0 and increase address?
     # # addi $t4, $t4, 2
-    #  li  $t5, 0             # reset t5 to 0 and increase address?
+    #  li  $t5, 0              # reset t5 to 0 and increase address?
     # li $t6, 0
-    # # addi $t5, $t5, 0
+    #  #addi $t5, $t5, 0
     #  #addi $t6, $t6, 0
     #  #la $t1, frame0
     #  #addi $t1, $t1, 2 
 
 
-    # addi $s7, $s7, 1    #If the frame has not reached every spot loop again
+    # addi $s7, $s7, 1         # If the frame has not reached every spot loop again
     #  bne $s7, 4, loopi
 
     j      print_result            
@@ -79,31 +80,38 @@ vbsme:
 
     loopj:
       
-    sll    $t7, $t5, 1       #16 for columns (Change this to to use $a whatever register length, I just used 1 to test a0)
-    add    $t7, $t7, $t6     #(i*16) + j
-    sll    $t7, $t7, 2       # 4 * ((i * 16) + j)  datasize of 4
-    add    $t9, $t7, $t0     # address of A[i][j] A is window 
-    lw     $t9, 0($t9)       # value of A[i][j]
+    sll    $t7, $t5, 1        #16 for columns (Change this to to use $a whatever register length, I just used 1 to test a0)
+    add    $t7, $t7, $t6      #(i*16) + j
+    sll    $t7, $t7, 2        # 4 * ((i * 16) + j)  datasize of 4
+    add    $t9, $t7, $t0      # address of A[i][j] A is window 
+    lw     $t9, 0($t9)        # value of A[i][j]
 
     sll    $t4, $t6, 2         #change this to the column size of frame?? (B is frame)      
     add    $t4, $t4, $t5
-    sll    $t4, $t4, 2       # 4 * ((j * 16) + i)
-    add    $t4, $t4, $t1     # address of B[j][i]
-    lw     $t4, 0($t4)       # value of B[j][i]
+    sll    $t4, $t4, 2        # 4 * ((j * 16) + i)
+    add    $t4, $t4, $t1      # address of B[j][i]
+    lw     $t4, 0($t4)        # value of B[j][i]
+        
 # put sum of differences into c and store stuff? ??  ? ?? ? ? ? 
+        
     sub    $t4, $t4, $t9     # A[i][j] - B[j][i] 
+        
     #absolute value delete this below
     sra $t3,$t4,31   
-    xor $t4,$t4,$t3   #I have not used t3? t4 is output
+    xor $t4,$t4,$t3        #I have not used t3? t4 is output
     sub $t4,$t4,$t3 
     #absolute value delete this above (cant use sra?)
+
 #t4 stores total sum of differences of window - frame, now move frame and repeat on right side of window
     add    $t7, $t7, $t2     # address of C[i][j]
     sw     $t4, 0($t7)       # store answer into C[i][j] 
-    add    $v0, $v0, $t4 #stores total sum of difference for a window size of a, compare this val with other windows in a different spot
+    add    $v0, $v0, $t4     #stores total sum of difference for a window size of a, compare this val with other windows in a different spot
+       
         #add code somewhere, if new v0 < old v0 then get index of top left address, dont use v0 either
+            
     addi   $t6, $t6, 1       # j++
     bne    $s5, $t6, loopj   # continue loopj if j < window columns
+        
     #returns back to i
     jr $ra
 
