2cc96557d02518779eb568144b576921c1e752b1
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-05 15:09:42 -0700

    Update DataMemory.v

--- DataMemory.v
+++ DataMemory.v
@@ -37,14 +37,34 @@
 
 module DataMemory(Address, WriteData, Clk, MemWrite, MemRead, ReadData); 
 
-    input [31:0] Address; 	// Input Address 
-    input [31:0] WriteData; // Data that needs to be written into the address 
+    input [31:0] Address; 	    // Input Address 
+    input [31:0] WriteData;     // Data that needs to be written into the address 
     input Clk;
-    input MemWrite; 		// Control signal for memory write 
-    input MemRead; 			// Control signal for memory read 
+    input MemWrite; 		    // Control signal for memory write 
+    input MemRead; 			    // Control signal for memory read 
 
-    output reg[31:0] ReadData; // Contents of memory location at Address
+    output reg[31:0] ReadData;  // Contents of memory location at Address
 
     /* Please fill in the implementation here */
+
+    reg [31:0] memory [0:1023];                // an array of 1024 (1K)
+
+    // The 'WriteData' value is written into the address corresponding
+    // to Address[11:2] in the positive clock edge if 'MemWrite' signal is 1
+    always @(posedge Clk) begin
+        if (MemWrite == 1'b1) begin
+            memory[Address[11:2]] <= WriteData;
+        end
+    end    
+
+    // 'ReadData' is the value of memory location Address[11:2] if 'MemRead'
+    // is 1, otherwise, it is 0x00000000. The reading of memory is not clocked.
+    always @(*) begin
+        if (MemRead == 1'b1) begin
+                ReadData <= memory[Address[11:2]];
+        end
+        else
+                ReadData <= 32'h0;    
+    end   
 
 endmodule
