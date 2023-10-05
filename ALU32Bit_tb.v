d3f71dd8140d1bd73a8e3df79beb103319c63299
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-05 15:04:39 -0700

    Update ALU32Bit_tb.v

--- ALU32Bit_tb.v
+++ ALU32Bit_tb.v
@@ -25,7 +25,41 @@ module ALU32Bit_tb();
 
 	initial begin
 	
-    /* Please fill in the implementation here... */
+// addition
+	//case 1
+   	 A <= 32'h000003E8;
+   	 B <= 32'h00000112;
+   	 ALUControl <=4'b0000;
+   	 #50;
+   	 //case 2
+   	 A <= 32'hffff0000;
+   	 B <= 32'h1000000f;
+   	 ALUControl <=4'b0000;
+   	 #50;
+    
+ // subtraction
+  	 //case 5
+  	 A <= 32'h000003E8;
+  	 B <= 32'h00000112;
+   	 ALUControl <= 4'b0001;
+   	 #50;
+   	 //case 6
+   	 A <= 32'h00000112;
+	 B <= 32'h000003E8;
+   	 ALUControl <= 4'b0001;
+   	 #50;
+    
+ // multiplication
+   	 //case 10
+   	 A <= 32'h000003E8;
+   	 B <= 32'h00000112;
+   	 ALUControl <= 4'b0010;
+  	 #50;
+ 	 //case 11
+  	 A <= 32'hFFFF0000;
+   	 B <= 32'h0000000F;
+  	 ALUControl <= 4'b0010;
+  	 #50;
 	
 	end
 
