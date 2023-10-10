9f17e636c5cd6264f5cc9b754e5b2f20507721b6
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-09 17:12:42 -0700

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -51,5 +51,13 @@
 module RegisterFile(ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegWrite, Clk, ReadData1, ReadData2);
 
 	/* Please fill in the implementation here... */
+	input [4:0] ReadRegister1, ReadRegister2, WriteRegister;
+	input [31:0] WriteData;
+	input RegWrite;
+	input Clk;
+
+	output [31:0] ReadData1, ReadData2;
+
+	
 
 endmodule
