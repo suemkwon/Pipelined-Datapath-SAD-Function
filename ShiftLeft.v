fe2f0c14c5896276b2d4320085f0f97c14d099b5
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-25 06:31:18 -0700

    Update ShiftLeft.v

--- ShiftLeft.v
+++ ShiftLeft.v
@@ -1,1 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/25/2023 05:05:51 AM
+// Design Name: 
+// Module Name: ShiftLeft
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
+
+module ShiftLeft(in, out);
+
+    input [31:0] in;
+    output reg [31:0] out;
+    
+    initial begin
+        out <= 0;
+    end
+    
+    always @(*) begin
+        out <= in << 2;
+     end
+        
+endmodule
