d49a33b83600ee82618f61d460697dbffeff3fab
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-27 12:43:54 -0700

    Update ProgramCounter.v

--- ProgramCounter.v
+++ ProgramCounter.v
@@ -24,29 +24,19 @@
 // location, 0x00000000H).
 ////////////////////////////////////////////////////////////////////////////////
 
- module ProgramCounter(Address, PCResult, Reset, Clk);
-    
-    // 32-Bit address input port
-	input [31:0] Address; 
-	// 1-Bit input control signal, 1-Bit input clock signal
-	input Reset, Clk; 
-	
-    // 32-Bit registered output port
-	output reg [31:0] PCResult; 
-	
-    // Global reset of datapath
-    initial begin 
-        PCResult <= 32'h00000000;
-    end
-    
-    //Updates at positive edge of the clock or 'X' upon instantiation
-    always @(posedge Clk, posedge Reset) begin 
-        if (Reset == 1)
-            //Register defaults to unknown values
-            PCResult <= 32'h00000000; 
-        else 
-            //Register holds the current address of the instruction memory 
-            PCResult <= Address; 
-    end    
+module ProgramCounter(Address, PCResult, rst, clk);
 
+	input [31:0] Address;
+	input rst, clk;
+	
+	output reg [31:0] PCResult;
+	
+	always @(posedge clk, posedge rst) begin
+	   if (rst == 1) begin
+	       PCResult <= 32'h00000000;
+	   end
+	   else
+	       PCResult <= Address;
+	end
+	
 endmodule
