61844b5b951dcee6778ea9e74d17d5ddf59935ea
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-28 10:57:10 -0700

    Update MEM_WB_REG.v

--- PipelinedRegisters/MEM_WB_REG.v
+++ PipelinedRegisters/MEM_WB_REG.v
@@ -1,78 +1,42 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
 // 
-// Create Date: 10/25/2023 04:15:24 AM
-// Design Name: 
-// Module Name: ID-EX
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
+// Module Name: MEM_WB_REG
 // 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
 // Additional Comments:
+//
+// Last Updated: 10:56 AM 10/28/23 by Sue
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module EX_MEM_REG(clk, RegWrite, Branch, MemWrite, MemRead, MemToReg, PCSrc, Zero, ALUResult, WriteMemData, pc_plus_4, WriteReg, 
-                    RegWrite_out, Branch_out, MemWrite_out, MemRead_out, MemToReg_out, PCSrc_out, Zero_out, 
-                    ALUResult_out, WriteMemData_out, pc_plus_4_out, WriteReg_out);
-                    
-        input clk;
-        input RegWrite, Branch, MemWrite, MemRead, MemToReg, PCSrc;
-        input Zero;
-        input [31:0] ALUResult;
-        input [31:0] WriteMemData, pc_plus_4;
-        input [4:0] WriteReg;
-        
-        output reg RegWrite_out, Branch_out, MemWrite_out, MemRead_out, MemToReg_out, PCSrc_out;
-        output reg Zero_out;
-        output reg [31:0] ALUResult_out;
-        output reg [31:0] WriteMemData_out, pc_plus_4_out;
-        output reg [4:0] WriteReg_out;
-        
-//        reg PRegWrite, PBranch, PMemWrite, PMemRead, PMemToReg;
-        
-        initial begin
-            RegWrite_out = 0;
-            Branch_out = 0;
-            MemWrite_out = 0;
-            MemRead_out = 0;
-            MemToReg_out = 0;        
-            pc_plus_4_out = 0;
-            Zero_out = 0;
-            ALUResult_out = 0;
-            WriteMemData_out = 0;
-            WriteReg_out = 0;
-            PCSrc_out = 0;
-        end
-        
-//        always @(posedge clk)   begin
-//        PRegWrite <= RegWrite;
-//        PBranch <= Branch;
-//        PMemWrite <= MemWrite;
-//        PMemRead <= MemRead;
-//        PMemToReg <= MemToReg;   
-//        end
-        
-        always @(posedge clk)   begin
-            pc_plus_4_out <= pc_plus_4;
-            Zero_out <= Zero;
-            ALUResult_out <= ALUResult;
-            WriteMemData_out <= WriteMemData;
-            WriteReg_out <= WriteReg;
-            RegWrite_out <= RegWrite;
-            Branch_out <= Branch;
-            MemWrite_out <= MemWrite;
-            MemRead_out <= MemRead;
-            MemToReg_out <= MemToReg;
-            PCSrc_out <= PCSrc;
-        end       
-        
+module MEM_WB_REG(clk, RegWrite, MemToReg, MemData, ALUResult, WriteReg, RegWrite_out, 
+                    MemToReg_out, MemData_out, ALUResult_out, WriteReg_out);
+
+    input [31:0] MemData;
+    input [31:0] ALUResult;
+    input [4:0] WriteReg;
+    input clk, RegWrite, MemToReg;
+    
+    output reg [31:0] MemData_out;
+    output reg [31:0] ALUResult_out;
+    output reg [4:0] WriteReg_out;
+    output reg RegWrite_out, MemToReg_out;
+    
+    initial begin
+        RegWrite_out = 0;
+        MemToReg_out = 0;
+        MemData_out = 0;
+        ALUResult_out = 0;
+        WriteReg_out = 0;
+    end  
+    
+    always @(posedge clk) begin
+        MemData_out <= MemData;
+        ALUResult_out <= ALUResult;
+        WriteReg_out <= WriteReg;
+        RegWrite_out <= RegWrite;
+        MemToReg_out <= MemToReg;
+    end     
+      
 endmodule
