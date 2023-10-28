bda526822eeed14244d2d6c1add4a95f2dce6201
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-28 10:37:58 -0700

    Update Mux32Bit2To1.v

--- Mux32Bit2To1.v
+++ Mux32Bit2To1.v
@@ -1,26 +1,29 @@
 `timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
-// ECE369 - Computer Architecture
+//////////////////////////////////////////////////////////////////////////////////
 // 
-// Module - Mux32Bit2To1.v
+// Module Name: Mux32Bit2To1
+//
 // Description - Performs signal multiplexing between 2 32-Bit words.
-////////////////////////////////////////////////////////////////////////////////
+// 
+// Additional Comments:
+//
+// Last Updated: 10:37 AM 10/28/23 by Sue
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module Mux32Bit2To1(out, inA, inB, sel);
-
-    output reg [31:0] out;
     
-    input [31:0] inA;
-    input [31:0] inB;
+    input [31:0] inA, inB;
     input sel;
+    
+    output reg [31:0] out;
 
-    /* Fill in the implementation here ... */ 
     always @ (sel, inA, inB) begin
         if (sel)
             out <= inA;
         else
             out <= inB;
-    end
-
+    end   
+           
 endmodule
