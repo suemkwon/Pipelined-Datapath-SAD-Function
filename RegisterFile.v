1301bebdaa832f59fcc1cc7b612d0973209dc61e
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-28 10:27:48 -0700

    Update RegisterFile.v

--- RegisterFile.v
+++ RegisterFile.v
@@ -1,17 +1,8 @@
 `timescale 1ns / 1ps
-
-////////////////////////////////////////////////////////////////////////////////
-// ECE369 - Computer Architecture
+//////////////////////////////////////////////////////////////////////////////////
 // 
+// Module Name: RegisterFile
 //
-//
-// Student(s) Name and Last Name: Justyn Arnold (33%), George Collias (33%), Sue Kwon (33%)
-//
-//
-// Module - register_file.v
-// Description - Implements a register file with 32 32-Bit wide registers.
-//
-// 
 // INPUTS:-
 // ReadRegister1: 5-Bit address to select a register to be read through 32-Bit 
 //                output port 'ReadRegister1'.
@@ -46,38 +37,42 @@
 // We will design the register file such that the contents of registers do not 
 // change for a pre-specified time before the falling edge of the clock arrives 
 // to allow for data multiplexing and setup time.
-////////////////////////////////////////////////////////////////////////////////
+// 
+// Additional Comments:
+//
+// Last Updated: 10:26 AM 10/28/23 by Sue
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 
 module RegisterFile(clk, ReadRegister1, ReadRegister2, ReadData1, ReadData2, WriteRegister, WriteData, RegWrite);
     
     input clk;
-    input [4:0] ReadRegister1, ReadRegister2;
-    input [4:0] WriteRegister;
+    input [4:0] ReadRegister1, ReadRegister2, WriteRegister;
     input [31:0] WriteData;
     input RegWrite;
     
     integer i;
     
     output reg [31:0] ReadData1, ReadData2;
-    reg [31:0] Registers [31:0];  
+    reg [31:0] Reg [31:0];  
     
     initial begin
         for (i = 0; i < 32; i=i+1) begin
-            Registers[i] = 0;
+            Reg[i] = 0;
         end
-     end       
-        	
+     end             	
 	
-	always @(clk or WriteData or WriteRegister or RegWrite)  begin
+	always @(clk or WriteData or WriteRegister or RegWrite) begin
 	   if(clk && RegWrite) begin
-	       Registers[WriteRegister] <= WriteData;
+	       Reg[WriteRegister] <= WriteData;
 	   end
 	end
 	
-	always @(clk or ReadRegister1 or ReadRegister2 or WriteRegister)  begin
-	   if(~clk)begin
-	   	   ReadData1 <= Registers[ReadRegister1];
-           ReadData2 <= Registers[ReadRegister2];    
+	always @(clk or ReadRegister1 or ReadRegister2 or WriteRegister) begin
+	   if(~clk) begin
+	   	   ReadData1 <= Reg[ReadRegister1];
+           ReadData2 <= Reg[ReadRegister2];    
 	   end
 	end             
 
