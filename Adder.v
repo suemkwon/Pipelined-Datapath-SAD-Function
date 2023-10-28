f031e2f426f28d4c5e2255fe9dae3732dc1b12be
suemkwon <136204147+suemkwon@users.noreply.github.com>
2023-10-28 10:49:40 -0700

    Update Adder.v

--- Adder.v
+++ Adder.v
@@ -1,21 +1,11 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
 // 
-// Create Date: 10/25/2023 05:05:51 AM
-// Design Name: 
 // Module Name: Adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
 // 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
 // Additional Comments:
+//
+// Last Updated: 10:49 AM 10/28/23 by Sue
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
