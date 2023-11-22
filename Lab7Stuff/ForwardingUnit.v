1f874fe00f14442e3e70bb77416a17b0a814f59a
gcollias <145624169+gcollias@users.noreply.github.com>
2023-11-22 01:36:52 -0700

    Update ForwardingUnit.v

--- Lab7Stuff/ForwardingUnit.v
+++ Lab7Stuff/ForwardingUnit.v
@@ -19,43 +19,45 @@ input branchEX;
  end
  
  
-  always @(negedge clk) begin
+  always @* begin
   //mem hazards
   
              bottomMuxOut = 0;
              topMuxOut = 0; 
              
      //mem/wb hazards        
-//    if((writeRegWB != 0) && (regwriteWB != 0) && (writeRegWB == regRSEX) && !((regwriteMEM != 0) && (writeRegMEM != 0) && (writeRegMEM == regRSEX))) begin
-//            topMuxOut = 1;
-//        end
-        
-//         if((writeRegWB != 0) && (regwriteWB != 0) && (writeRegWB == regRTEX) && !((regwriteMEM != 0) && (writeRegMEM != 0) && (writeRegMEM == regRTEX))) begin
-//            bottomMuxOut = 2;
-//         end
-//         //ex/mem hazards
-//        if((regwriteMEM != 0) && (writeRegMEM != 0) && (writeRegMEM == regRTEX)) begin
-//            bottomMuxOut = 1;
-//         end 
-         
-//       if((regwriteMEM != 0) && (writeRegMEM != 0) && (writeRegMEM == regRSEX)) begin
-//            topMuxOut = 2;
-//         end 
-             if((((writeRegWB != 0) && (regwriteWB != 0)) || (branchEX == 1))&& (writeRegWB == regRSEX) && !((((regwriteMEM != 0) && (writeRegMEM != 0)) || (branchEX == 1)) && (writeRegMEM == regRSEX))) begin
+    if((writeRegWB != 0) && (regwriteWB != 0) && (writeRegWB == regRSEX) && !((regwriteMEM != 0) && (writeRegMEM != 0) && (writeRegMEM == regRSEX))) begin
             topMuxOut = 1;
         end
         
-         if((((writeRegWB != 0) && (regwriteWB != 0)) || (branchEX == 1)) && (writeRegWB == regRTEX) && !(((((regwriteMEM != 0) && (writeRegMEM != 0)) || (branchEX == 1)) && (writeRegMEM == regRTEX)))) begin
-            bottomMuxOut = 2;
+         if((writeRegWB != 0) && (regwriteWB != 0) && (writeRegWB == regRTEX) && !((regwriteMEM != 0) && (writeRegMEM != 0) && (writeRegMEM == regRTEX))) begin
+            bottomMuxOut = 1;
          end
          //ex/mem hazards
-       if((((regwriteMEM != 0) && (writeRegMEM != 0)) || (branchEX == 1)) && (writeRegMEM == regRTEX)) begin
-            bottomMuxOut = 1;
-         end  
+        if((regwriteMEM != 0) && (writeRegMEM != 0) && (writeRegMEM == regRTEX)) begin
+            bottomMuxOut = 2;
+         end 
          
-       if((((regwriteMEM != 0) && (writeRegMEM != 0)) || (branchEX == 1)) && (writeRegMEM == regRSEX)) begin
+       if((regwriteMEM != 0) && (writeRegMEM != 0) && (writeRegMEM == regRSEX)) begin
             topMuxOut = 2;
          end 
+         
+         
+//             if((((writeRegWB != 0) && (regwriteWB != 0)) || (branchEX == 1))&& (writeRegWB == regRSEX) && !((((regwriteMEM != 0) && (writeRegMEM != 0)) || (branchEX == 1)) && (writeRegMEM == regRSEX))) begin
+//            topMuxOut <= 1;
+//        end
+        
+//         if((((writeRegWB != 0) && (regwriteWB != 0)) || (branchEX == 1)) && (writeRegWB == regRTEX) && !(((((regwriteMEM != 0) && (writeRegMEM != 0)) || (branchEX == 1)) && (writeRegMEM == regRTEX)))) begin
+//            bottomMuxOut <= 2;
+//         end
+//         //ex/mem hazards
+//       if((((regwriteMEM != 0) && (writeRegMEM != 0)) || (branchEX == 1)) && (writeRegMEM == regRTEX)) begin
+//            bottomMuxOut <= 1;
+//         end  
+         
+//       if((((regwriteMEM != 0) && (writeRegMEM != 0)) || (branchEX == 1)) && (writeRegMEM == regRSEX)) begin
+//            topMuxOut <= 2;
+//         end 
                      
       
     end
